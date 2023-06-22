632b1496b8b08636b532a65a41383a6c5f70ee68
amalecki <amalecki@RK-38>
2023-06-22 12:55:47 +0200

    Dodanie 2 modeli historycznych CarInsurance i CarRapair które do celowo podłączę relacją

---
+++ CarInformator/Models/Historian/CarInsuranceHistorian.cs
@@ -0,0 +1,10 @@
+﻿namespace CarInformator.Models.Historian
+{
+    public class CarInsuranceHistorian
+    {
+        public int Id { get; set; }
+        public bool AccidentFree { get; set; }
+        public int PrevOwner { get; set; }
+        public int Milage { get; set; }
+    }
+}
---
+++ CarInformator/Models/Historian/CarRepairHistorian.cs
@@ -0,0 +1,13 @@
+﻿namespace CarInformator.Models.Historian
+{
+    public class CarRepairHistorian
+    {
+        public int Id { get; set; }
+        public string RepiarName { get; set; }
+        public string RepiarDesc { get; set; }
+        public decimal Price { get; set; }
+        public string Date { get; set; }
+
+
+    }
+}
